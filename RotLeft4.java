7b01a0bbde2cbbce58aa7420feb8aa42b48e4585
favourehi <134142357+favourehi@users.noreply.github.com>
2024-06-11 15:46:02 +0100

    Add files via upload
    OTAKWU FAVOUR EHI
    BHU/22/04/10/0008

---
+++ RotLeft4.java
@@ -0,0 +1,11 @@
+public class RotLeft {
+    public static void main(String[] args) {
+
+        // 4. Printing Rotated Array
+        System.out.println();
+        System.out.println("Array after rotation: :");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
