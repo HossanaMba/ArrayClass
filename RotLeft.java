a148545fd7d3fe389f461433368cb431c4f2b0f7
Hossana  Mba <129768647+HossanaMba@users.noreply.github.com>
2024-06-11 15:26:32 +0100

    Add files via upload
    Mba Hossana 
    BHU/22/04/05/0053

---
+++ RotLeft.java
@@ -0,0 +1,9 @@
+public class RotLeft {
+    public static void main(String[] args) {
+        // 1. Initialization
+        int arr[] = new int[]{1, 2, 3, 4, 5};
+        int n = 4;
+
+        
+    }
+}
